da1d6c85c18830eb74f30b77084d9f6adc48ffea
geethagunaraj <gunarajgeetha@gmail.com>
2022-02-22 08:36:12 +0530

    one

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -4,6 +4,15 @@ public class Employee {
 public void empid(float empid) {
 	System.out.println("empid is:" + empid);
 }
+private void empName(String empName) {
+	System.out.println("empName is:" + empName);
+}
+private void five() {
+
+}
+
+
+
 public static void main(String[] args) {
 	Employee e = new Employee();
 	e.empid(15988);
