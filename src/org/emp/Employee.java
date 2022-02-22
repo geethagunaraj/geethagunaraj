b3a38030261c77e8f501fc24855221a0e7c28a5f
geethagunaraj <gunarajgeetha@gmail.com>
2022-02-22 08:28:18 +0530

    git2

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -4,6 +4,13 @@ public class Employee {
 public void empid(float empid) {
 	System.out.println("empid is:" + empid);
 }
+private void empName(String empName) {
+	System.out.println("empName is:" + empName);
+}
+
+private void empPhone(long empPhone) {
+System.out.println("empPhone is:"+empPhone);
+}
 public static void main(String[] args) {
 	Employee e = new Employee();
 	e.empid(15988);
