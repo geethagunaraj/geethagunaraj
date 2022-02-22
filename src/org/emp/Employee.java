e7a1f39cbe00338f65a3ac271ff7d12994ad8843
geethagunaraj <gunarajgeetha@gmail.com>
2022-02-22 07:24:58 +0530

    My Task

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,13 @@
+package org.emp;
+
+public class Employee {
+public void empid(float empid) {
+	System.out.println("empid is:" + empid);
+}
+public static void main(String[] args) {
+	Employee e = new Employee();
+	e.empid(15988);
+}
+	
+}
+
